f631b866e6285a5e7ff390415bb693f649dbc811
Aura <122836266+Aura2k@users.noreply.github.com>
2023-01-19 21:59:38 +0100

    Added JS functionality

---
+++ main.js
@@ -0,0 +1,71 @@
+let minuteTime = document.getElementById("minutes");
+let secondTime = document.getElementById("seconds");
+let millisecondTime = document.getElementById("milliseconds");
+
+let startBtn = document.getElementById("startBtn");
+let stopBtn = document.getElementById("stopBtn");
+let resetBtn = document.getElementById("resetBtn");
+
+let minutes = 00;
+let seconds = 00;
+let time = 00;
+let timer = false;
+
+startBtn.addEventListener('click', function () {
+    timer = true;
+    stopWatch();
+})
+
+stopBtn.addEventListener('click', function () {
+    timer = false;
+})
+
+resetBtn.addEventListener('click', function () {
+    timer = false;
+    minutes = 00;
+    seconds = 00;
+    time = 00;
+
+    minuteTime.innerHTML = "00";
+    secondTime.innerHTML = "00";
+    millisecondTime.innerHTML = "00";
+})
+
+
+function stopWatch() {
+    if(timer) {
+        time++;
+
+        if(time == 100) {
+            seconds++;
+            time = 0;
+        }
+
+        if (seconds == 60) {
+            minutes++;
+            seconds = 0;
+        }
+
+        let minuteString = minutes;
+        let secondString = seconds;
+        let millisecondString = time;
+
+        if(time < 10) {
+            millisecondString = "0" + time;
+        } 
+
+        if(seconds < 10) {
+            secondString = "0" + seconds;
+        }
+
+        if(minutes < 10) {
+            minuteString = "0" + minutes;
+        }
+
+        minuteTime.innerHTML = minuteString;
+        secondTime.innerHTML = secondString;
+        millisecondTime.innerHTML = millisecondString;
+
+        setTimeout(stopWatch, 10);
+    }
+}
